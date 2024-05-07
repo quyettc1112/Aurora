62d2529c5125c5e4c76e830c25b3abe61cae26f2
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-05-07 17:53:30 +0700

    Settup Base Config

---
+++ app/src/main/java/com/aurora/aurora/AppConfig/CustomView/CustomBottomNav/OnItemLongClickListener.kt
@@ -0,0 +1,5 @@
+package com.aurora.aurora.AppConfig.CustomView.CustomBottomNav
+
+interface OnItemLongClickListener {
+    fun onItemLongClick(pos: Int)
+}
---
+++ app/src/main/java/com/aurora/aurora/AppConfig/CustomView/CustomBottomNav/OnItemReselectedListener.kt
@@ -0,0 +1,5 @@
+package com.aurora.aurora.AppConfig.CustomView.CustomBottomNav
+
+interface OnItemReselectedListener {
+    fun onItemReselect(pos: Int)
+}
