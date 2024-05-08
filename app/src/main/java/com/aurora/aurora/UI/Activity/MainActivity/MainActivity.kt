43d9837c63a94cef3cf4f245da3d0d2809588cfd
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-05-08 16:55:45 +0700

    Update Splash Srceen APP

--- app/src/main/java/com/aurora/aurora/UI/Activity/MainActivity/MainActivity.kt
+++ app/src/main/java/com/aurora/aurora/UI/Activity/MainActivity/MainActivity.kt
@@ -3,7 +3,9 @@ package com.aurora.aurora.UI.Activity.MainActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aurora.aurora.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
