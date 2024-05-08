daae5b58dc39957db7b3f049fb7ed8346e9ddf33
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-05-08 16:33:49 +0700

    Update Custom View

--- app/src/main/java/com/aurora/aurora/AppConfig/Application/AuroraApp.kt
+++ app/src/main/java/com/aurora/aurora/AppConfig/Application/AuroraApp.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AuroraApp: BaseActivity() {
+class AuroraApp: Application() {
     override fun onCreate() {
         super.onCreate()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
