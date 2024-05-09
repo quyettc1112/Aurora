b641c06fcc24566a0560bdb1dd33340c50db1f9a
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-05-09 23:47:33 +0700

    Update Some logic testing

--- app/src/main/java/com/aurora/aurora/UI/Fragment/ProfileFragment/ProfileFragment.kt
+++ app/src/main/java/com/aurora/aurora/UI/Fragment/ProfileFragment/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.aurora.aurora.UI.Fragment.ProfileFragment
 
+import android.content.Intent
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.aurora.aurora.R
+import com.aurora.aurora.UI.Activity.AuthActivity.LoginActivity.LoginActivity
+import com.aurora.aurora.databinding.ActivityRegisterBinding
+import com.aurora.aurora.databinding.FragmentHomeBinding
+import com.aurora.aurora.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
-
-    private val viewModel: ProfileViewModel by viewModels()
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +27,13 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(requireActivity(), LoginActivity::class.java)
+            requireActivity().startActivity(intent)
+        }
+
+        return binding.root
     }
 }
