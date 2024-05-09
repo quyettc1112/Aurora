d979e11dce9353feb224bcddfce5b3cbc87c808a
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-05-09 23:33:34 +0700

    Update Bottom nav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.2.0")
     implementation("androidx.activity:activity:1.9.0")
     implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     /*    implementation("androidx.annotation:annotation-jvm:1.7.1")*/
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
---
+++ app/src/main/java/com/aurora/aurora/UI/Fragment/CartFragment/CartFragment.kt
@@ -0,0 +1,31 @@
+package com.aurora.aurora.UI.Fragment.CartFragment
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aurora.aurora.R
+
+class CartFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CartFragment()
+    }
+
+    private val viewModel: CartViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
+}
