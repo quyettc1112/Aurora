452399e1adc4a1203e5c4918144405ae46212361
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-05-11 23:37:04 +0700

    Update Home UI

--- app/src/main/java/com/aurora/aurora/Common/Constant/Constant.kt
+++ app/src/main/java/com/aurora/aurora/Common/Constant/Constant.kt
@@ -1,5 +1,8 @@
 package com.aurora.aurora.Common.Constant
 
+import com.aurora.aurora.Common.CommonAdapter.VideoMainAdapter
+import com.aurora.aurora.R
+
 class Constant {
 
     companion object{
@@ -17,5 +20,46 @@ class Constant {
         const val DEFAULT_PRIMARY_COLOR = "#FF4949"
         const val DEFAULT_PRIMARY_COLOR_ACTIVE = "#C9C9C9"
 
+
+        fun getListCourse(): List<VideoMainAdapter.Course> {
+            return listOf(
+                VideoMainAdapter.Course(
+                    title = "Độ tuổi dễ bị tổn thương sâu sắc về tình cảm",
+                    image = R.drawable.ic_material_aytism,
+                    rating = 4.5,
+                    reviews = 4242,
+                    originalPrice = 400000.0,
+                    discountedPrice = 350000.0
+                ),
+                VideoMainAdapter.Course(
+                    title = "Độ tuổi dễ bị tổn thương sâu sắc về tình cảm",
+                    image = R.drawable.ic_material_aytism,
+                    rating = 4.5,
+                    reviews = 4242,
+                    originalPrice = 400000.0,
+                    discountedPrice = 350000.0
+                ),
+
+                VideoMainAdapter.Course(
+                    title = "Độ tuổi dễ bị tổn thương sâu sắc về tình cảm",
+                    image = R.drawable.ic_material_aytism,
+                    rating = 4.5,
+                    reviews = 4242,
+                    originalPrice = 400000.0,
+                    discountedPrice = 350000.0
+                ),
+
+                VideoMainAdapter.Course(
+                    title = "Độ tuổi dễ bị tổn thương sâu sắc về tình cảm",
+                    image = R.drawable.ic_material_aytism,
+                    rating = 4.5,
+                    reviews = 4242,
+                    originalPrice = 400000.0,
+                    discountedPrice = 350000.0
+                )
+            )
+
+
+        }
     }
 }
