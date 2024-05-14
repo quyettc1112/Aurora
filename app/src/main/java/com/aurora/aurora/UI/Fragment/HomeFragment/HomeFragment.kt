3988b0a51b1e1967ff96d4b05f885f9f4c5841cd
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-05-14 17:02:42 +0700

    Update Home UI

--- app/src/main/java/com/aurora/aurora/UI/Fragment/HomeFragment/HomeFragment.kt
+++ app/src/main/java/com/aurora/aurora/UI/Fragment/HomeFragment/HomeFragment.kt
@@ -5,8 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.aurora.aurora.Common.CommonAdapter.VideoMainAdapter
 import com.aurora.aurora.Common.Constant.Constant
+import com.aurora.aurora.R
 import com.aurora.aurora.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -28,6 +35,9 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
         setUpVideoMainRecycleView()
 
+        setIndicator()
+        setCurrentIndicator(0)
+
         return binding.root
     }
 
@@ -36,5 +46,62 @@ class HomeFragment : Fragment() {
 
     private fun setUpVideoMainRecycleView(){
         binding.rvVideo.adapter = videoAdapter
+
+        binding.rvVideo.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+                setCurrentIndicator(firstVisibleItemPosition)
+            }
+        })
+    }
+
+
+    private fun setIndicator() {
+        val indicator = arrayOfNulls<ImageView>(videoAdapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        layoutParams.setMargins(15,0,15,0)
+        for (i in indicator.indices) {
+            indicator[i] = ImageView(requireContext())
+            indicator[i].apply {
+                this?.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.indicator_inactive
+                    )
+                )
+                this?.layoutParams = layoutParams
+            }
+            binding.indicatorContainer.addView(indicator[i])
+        }
+
+    }
+
+    private fun setCurrentIndicator(index: Int) {
+        val childCount = binding.indicatorContainer.childCount
+        for (i in 0 until  childCount) {
+            val imageView = binding.indicatorContainer[i] as ImageView
+            if (i == index) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.indicator_active
+                    )
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.indicator_inactive
+                    )
+                )
+            }
+        }
+
     }
 }
