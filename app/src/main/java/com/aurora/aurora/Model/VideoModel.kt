09f47a5595e579a4108481b869d792311b420328
Admin <Admin@MSI>
2024-05-27 14:01:09 +0700

    Update Video UI and Logic URI

--- app/src/main/java/com/aurora/aurora/Model/VideoModel.kt
+++ app/src/main/java/com/aurora/aurora/Model/VideoModel.kt
@@ -3,7 +3,7 @@ package com.aurora.aurora.Model
 import android.net.Uri
 
 data class VideoModel(
-    private val titleVideo: String,
-    private val descriptionVideo: String,
-    private val uriVideo: Uri
+     val titleVideo: String,
+     val descriptionVideo: String,
+     val uriVideo: Uri
 )
