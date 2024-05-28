c4123ad8d0b9cdf7cf032efd231ab3847344fd4e
Admin <Admin@MSI>
2024-05-28 19:03:42 +0700

    Done Show Video Rycecle View

--- app/src/main/java/com/aurora/aurora/Model/VideoModel.kt
+++ app/src/main/java/com/aurora/aurora/Model/VideoModel.kt
@@ -3,7 +3,5 @@ package com.aurora.aurora.Model
 import android.net.Uri
 
 data class VideoModel(
-     val titleVideo: String,
-     val descriptionVideo: String,
-     val Image: Int
+     val id: String,
 )
