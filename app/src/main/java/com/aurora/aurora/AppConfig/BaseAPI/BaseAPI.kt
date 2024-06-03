26fae1ee903a383e8d3da5d45162c1cfa98c75f5
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-06-03 15:09:07 +0700

    Ahhh i struggle with SSL

--- app/src/main/java/com/aurora/aurora/AppConfig/BaseAPI/BaseAPI.kt
+++ app/src/main/java/com/aurora/aurora/AppConfig/BaseAPI/BaseAPI.kt
@@ -4,6 +4,7 @@ class BaseAPI {
 
     companion object {
         const val BASE_YOUTUBE_URL: String = "https://www.googleapis.com/youtube/v3/"
+        const val BASE_API : String = "https://starci.net:1969"
         const val NETWORK_TIMEOUT = 60L
     }
 }
