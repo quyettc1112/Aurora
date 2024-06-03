4dca33cbc55ba8822259b1737c388a58188b66bb
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-06-03 15:08:59 +0700

    Ahhh i struggle with SSL

--- app/src/main/java/com/aurora/aurora/DI/APIModule.kt
+++ app/src/main/java/com/aurora/aurora/DI/APIModule.kt
@@ -1,5 +1,7 @@
 package com.aurora.aurora.DI
 
+import android.content.Context
+import com.aurora.aurora.API_Services.UserAPI_Service
 import com.aurora.aurora.API_Services.YoutubeAPI_Service
 import com.aurora.aurora.AppConfig.BaseAPI.BaseAPI
 import com.google.firebase.BuildConfig
@@ -8,12 +10,14 @@ import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -23,7 +27,13 @@ object APIModule {
 
     @Provides
     @Singleton
-    fun provideBaseUrl() = BaseAPI.BASE_YOUTUBE_URL
+    @Named("youtube")
+    fun provideYoutubeBaseUrl() = BaseAPI.BASE_YOUTUBE_URL
+
+    @Provides
+    @Singleton
+    @Named("user")
+    fun provideUserBaseUrl() = BaseAPI.BASE_API
 
     @Provides
     @Singleton
@@ -32,6 +42,8 @@ object APIModule {
     @Provides
     @Singleton
     fun provideGson(): Gson = GsonBuilder().setLenient().create()
+
+
 
 
     @Provides
@@ -53,7 +65,6 @@ object APIModule {
                 .build()
             return@Interceptor chain.proceed(request)
         }
-
         OkHttpClient
             .Builder()
             .addInterceptor(requestInterceptor)
@@ -66,17 +77,27 @@ object APIModule {
     }
 
 
-
-
     @Provides
     @Singleton
-    fun provideYoutubeAPI(baseUrl: String, gson: Gson, client: OkHttpClient): YoutubeAPI_Service =
+    fun provideYoutubeAPI(@Named("youtube") baseUrl: String, gson: Gson, client: OkHttpClient): YoutubeAPI_Service =
         Retrofit.Builder()
             .baseUrl(baseUrl)
             .client(client)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(YoutubeAPI_Service::class.java)
+
+
+
+    @Provides
+    @Singleton
+    fun provideUserAPI(@Named("user") baseUrl: String, gson: Gson, client: OkHttpClient): UserAPI_Service =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(UserAPI_Service::class.java)
 
 
 }
