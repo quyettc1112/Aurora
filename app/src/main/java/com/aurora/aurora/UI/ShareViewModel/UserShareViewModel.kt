8aecf4f0c621a4aef736e6a6903c385518117cb9
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-06-06 13:29:15 +0700

    Done Get JWT Token

--- app/src/main/java/com/aurora/aurora/UI/ShareViewModel/UserShareViewModel.kt
+++ app/src/main/java/com/aurora/aurora/UI/ShareViewModel/UserShareViewModel.kt
@@ -4,12 +4,20 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.aurora.aurora.Model.RequestDTO.UserCretidentialDTO
+import com.aurora.aurora.Model.Respone.JWTObject
 import com.google.auth.oauth2.UserCredentials
 
 class UserShareViewModel: ViewModel() {
     private val _userCredentials = MutableLiveData<UserCretidentialDTO>()
     val userCredentials: MutableLiveData<UserCretidentialDTO> get() = _userCredentials
 
+
+
+    private  val _jwtToken = MutableLiveData<JWTObject?>()
+    val jwtToken: MutableLiveData<JWTObject?> get() = _jwtToken
+
+
+    // User Cretidentail
     fun updateUserCretidential(userCretidentialDTO: UserCretidentialDTO) {
         _userCredentials.value = userCretidentialDTO
     }
@@ -17,4 +25,24 @@ class UserShareViewModel: ViewModel() {
     fun getUserCretidentail(): UserCretidentialDTO? {
         return userCredentials.value
     }
+
+    // User JWT
+
+    fun updateJWTToken(jwtObject: JWTObject) {
+        _jwtToken.value = jwtObject
+
+    }
+
+    fun getJWTToken(): JWTObject? {
+        return jwtToken.value
+    }
+
+    // Function to clear JWT Token
+    fun clearJWTToken() {
+        _jwtToken.value = null
+    }
+
+
+
+
 }
