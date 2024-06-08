1440960ca81029b5f8be3f74e3276882eea513e5
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-06-08 23:09:07 +0700

    Done Regsiter

--- app/src/main/java/com/aurora/aurora/API_Services/UserAPI_Service.kt
+++ app/src/main/java/com/aurora/aurora/API_Services/UserAPI_Service.kt
@@ -3,6 +3,7 @@ package com.aurora.aurora.API_Services
 import com.aurora.aurora.Model.RequestDTO.RegisterRequestDTO
 import com.aurora.aurora.Model.RequestDTO.UserCretidentialDTO
 import com.aurora.aurora.Model.Respone.JWTObject
+import com.aurora.aurora.Model.Respone.MessageRespone
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Header
@@ -19,6 +20,6 @@ interface UserAPI_Service {
     @POST("/auth/sign-up")
     fun callRegisterUser(
         @Body registerRequestDTO: RegisterRequestDTO
-    )
+    ): Call<MessageRespone>
 
 }
