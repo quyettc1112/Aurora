cb296f6f819c7333d39ee8097ab9848d595b58e4
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-06-09 16:39:24 +0700

    Update Product Detail

---
+++ app/src/main/java/com/aurora/aurora/UI/Activity/ProductDetailActivity/ProductDetailActivity.kt
@@ -0,0 +1,70 @@
+package com.aurora.aurora.UI.Activity.ProductDetailActivity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.aurora.aurora.AppConfig.BaseConfig.BaseActivity
+import com.aurora.aurora.Common.CommonAdapter.ImageReleventAdapter
+import com.aurora.aurora.Common.Constant.Constant
+import com.aurora.aurora.Model.ImageReleventModel
+import com.aurora.aurora.Model.ToyModel
+import com.aurora.aurora.R
+import com.aurora.aurora.UI.Activity.MainActivity.MainActivity
+import com.aurora.aurora.databinding.ActivityProductDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProductDetailActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityProductDetailBinding
+    private lateinit var toyModel: ToyModel
+
+    private lateinit var imageReleventAdapter: ImageReleventAdapter
+    private var listItemRelevent: ArrayList<ImageReleventModel> = ArrayList<ImageReleventModel>()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityProductDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setUpImageRelevent()
+        backToPreviousActivity()
+    }
+
+    private fun setUpImageRelevent() {
+        imageReleventAdapter = ImageReleventAdapter()
+        getToyModelByIDFromConstans()
+
+        if (toyModel != null) {
+            listItemRelevent.add(ImageReleventModel(0, toyModel.listImage.get(0)))
+            listItemRelevent.add(ImageReleventModel(1, toyModel.listImage.get(1)))
+            listItemRelevent.add(ImageReleventModel(2, toyModel.listImage.get(2)))
+            imageReleventAdapter.submitList(listItemRelevent)
+            binding.rlImageRelevent.adapter = imageReleventAdapter
+            imageReleventAdapter.onItemImageClickListener = {
+                binding.imToyImage.setImageResource(it.image)
+            }
+        }
+    }
+    private fun backToPreviousActivity() {
+        binding.customToolbar2.onStartIconClick = {
+            finish() // This will close the current activity and return to the previous one in the stack
+        }
+    }
+
+    private fun getToyModelByIDFromConstans() {
+        val id = intent.getIntExtra("product_id", 0);
+        toyModel = Constant.getListToys().get(id)
+    }
+
+
+}
